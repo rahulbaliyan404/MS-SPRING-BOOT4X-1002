5b0258dc7afb35aa529e23a217ced8da9e85055b
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2025-12-18 14:24:58 +0530

    changes

---
+++ src/main/java/com/home/MS_SPRING_BOOT4X_1002/MsSpringBoot4X1002Application.java
@@ -0,0 +1,13 @@
+package com.home.MS_SPRING_BOOT4X_1002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringBoot4X1002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringBoot4X1002Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/MS_SPRING_BOOT4X_1002/MsSpringBoot4X1002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.MS_SPRING_BOOT4X_1002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MsSpringBoot4X1002ApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
